1a696b9f170e8d23788af3bc69e4d3e84c05484e
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-06-13 18:39:18 -0300

    225. Cadastrando novas tarefas

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -52,7 +52,7 @@ class TarefaController extends Controller
      */
     public function create()
     {
-        //
+        return view('tarefa.create');
     }
 
     /**
@@ -60,7 +60,8 @@ class TarefaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+       $tarefa = Tarefa::create($request->all());
+       return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
     }
 
     /**
@@ -68,7 +69,7 @@ class TarefaController extends Controller
      */
     public function show(Tarefa $tarefa)
     {
-        //
+        return 'chegamos aqui';
     }
 
     /**
