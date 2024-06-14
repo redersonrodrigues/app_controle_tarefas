3a9db4c8228246e21268bfb98fb7377ebf94d077
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-06-14 12:57:15 -0300

    234. Melhorando a navegação

--- resources/views/tarefa/index.blade.php
+++ resources/views/tarefa/index.blade.php
@@ -6,9 +6,21 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                        Tarefas
+                        <div class="row">
+                            <div class="col-6">
+                                Tarefas
+                            </div>
+                            <div class="col-6">
+                                <div class="float-right">
+                                    <a href="{{route('tarefa.create')}}" class="mr-3">Novo</a>
+                                    {{-- <a href="{{route('tarefa.exportacao', ['extensao' => 'xlsx'])}}" class="mr-3">XLSX</a>
+                                    <a href="{{route('tarefa.exportacao', ['extensao' => 'csv'])}}" class="mr-3">CSV</a>
+                                    <a href="{{route('tarefa.exportacao', ['extensao' => 'pdf'])}}" class="mr-3">PDF</a>
+                                    <a href="{{route('tarefa.exportar')}}" target="_blank">PDF V2</a> --}}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
                     <div class="card-body">
                         <table class="table">
                             <thead>
